0ee7d804e716b448f81b3e7b9b5415fa95631ff6
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2022-04-08 18:51:25 +0300

    modified authentication

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -4,49 +4,68 @@ namespace App\Http\Controllers;
 use App\Models\person;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class authController extends Controller
 {
-     
+
     function register(Request $request){
-        $register = new person;
-        $register -> email = $request -> email;
-        $register -> password = $request -> password;
-        if($register -> save()){
-            $response = ["Message"=> "Registration successful"];
-            return response($response, 200);
-        } else {
-            $response = ["Message"=> "Registration failed"];
-            return response($response, 404);
+        // $register = new person;
+        // $register -> email = $request -> email;
+        // $register -> password = $request -> password;
+        // if($register -> save()){
+        //     $response = ["Message"=> "Registration successful"];
+        //     return response($response, 200);
+        // } else {
+        //     $response = ["Message"=> "Registration failed"];
+        //     return response($response, 404);
+        // }
+
+        $createuser = person::create(['email'=>$request->email, 'password'=>Hash::make($request->password)]);
+        if($createuser){
+            return response(['message'=>'success', 'data'=>$createuser], 200);
         }
-        
+        else{
+            return response(['message'=>'failure', 'data'=>$createuser], 404);
+        }
+
     }
 
 
     function login(Request $request){
-        $useremail = new person;
-        $useremail -> email = $request -> email;
-        $useremail -> password = $request -> password;
-        
-          $savedCredentials = person::get();
-          foreach($savedCredentials as $item){
-               
-            if($item->email == $useremail['email'] && $item->password == $useremail['password']){
-                
-                $response = ["Message"=> "Login successful"];
-                return response($response, 200 );
-                break;
-            }
-                else{
-                $response = ["Message"=> "Failed to login"];
-                return response($response, 404);
-               
-                }
-               
-    }   // foreach
+    //     $useremail = new person;
+    //     $useremail -> email = $request -> email;
+    //     $useremail -> password = $request -> password;
 
-    
-     
+    //       $savedCredentials = person::get();
+    //       foreach($savedCredentials as $item){
+
+    //         if($item->email == $useremail['email'] && $item->password == $useremail['password']){
+
+    //             $response = ["Message"=> "Login successful"];
+    //             return response($response, 200 );
+    //             break;
+    //         }
+    //             else{
+    //             $response = ["Message"=> "Failed to login"];
+    //             return response($response, 404);
+
+    //             }
+
+    // }   //
+
+
+    if(Auth::attempt(['email' => $request->email, 'password' =>$request->password])){
+
+
+        return response(['message'=>"success", "data"=>Auth::user()],200);
+    }else{
+        return response(['message'=>"failure", "data"=>"Login failed"],404);
+    }
+
+
+
     }
 
 
@@ -57,7 +76,7 @@ class authController extends Controller
         ]);
     }
 
-    
+
     public function user(Request $request){
         return response()->json($request->user());
     }
--- app/Models/person.php
+++ app/Models/person.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class person extends Model
+class person extends Authenticatable
 {
     public $table = 'persons';
     use HasFactory;
